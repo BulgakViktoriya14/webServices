56a30575614c0093be25577e2f203597981f67d0
BulgakViktoriya14 <bulgak.viktoriya@gmail.com>
2019-12-16 21:47:37 +0300

    add application aws-java-products-api

---
+++ lab3/aws-java-products-api/src/main/java/com/serverless/Handler.java
@@ -0,0 +1,32 @@
+package com.serverless;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+	private final static String BODY_KEY = "body";
+
+	private static final Logger LOG = LogManager.getLogger(Handler.class);
+
+	@Override
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+		LOG.info("received: {}", input);
+		String requestBody = (String) input.get(BODY_KEY);
+		String responseBody = filter(requestBody);
+		return ApiGatewayResponse.builder()
+				.setStatusCode(200)
+				.setObjectBody(responseBody)
+				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.build();
+	}
+
+	private String filter(String content) {
+		return "Сообщение:<br/><b>" + content+ "</b>";
+	}
+}
